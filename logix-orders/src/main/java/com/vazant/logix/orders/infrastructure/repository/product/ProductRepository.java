839a22afe3bcd91ab2258a88768a56561ff047ec
Dzmitry Patapau <dimaspotapov@gmail.com>
2025-07-07 03:48:05 +0200

    refactor: improve repository layer and application tests

--- logix-orders/src/main/java/com/vazant/logix/orders/infrastructure/repository/product/ProductRepository.java
+++ logix-orders/src/main/java/com/vazant/logix/orders/infrastructure/repository/product/ProductRepository.java
@@ -1,7 +1,17 @@
 package com.vazant.logix.orders.infrastructure.repository.product;
 
 import com.vazant.logix.orders.domain.product.Product;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ProductRepository extends JpaRepository<Product, UUID> {}
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+  
+  @Query("SELECT p FROM Product p WHERE p.categoryId = :categoryUuid")
+  List<Product> findByCategoryUuid(@Param("categoryUuid") UUID categoryUuid);
+  
+  @Query("SELECT p FROM Product p WHERE p.organization.uuid = :organizationUuid")
+  List<Product> findByOrganizationUuid(@Param("organizationUuid") UUID organizationUuid);
+}
